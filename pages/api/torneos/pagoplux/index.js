bca1f0f51a9d9c814ec8876192f307e127ad57a1
jose andre alcivar <alcivar372@gmail.com>
2024-09-27 21:07:24 -0400

    correcion json pagoplux

--- pages/api/torneos/pagoplux/index.js
+++ pages/api/torneos/pagoplux/index.js
@@ -4,12 +4,37 @@ import db from "/libs/db";
 
 export default async function pagoHandler(req, res) {
   try {
-    const data = { ...req.body };
-    data.idtransaccion = data.id_transaccion;
-    const { id_transaccion, ...pagoData } = data;
+    // const data = { ...req.body };
+    // data.idtransaccion = data.id_transaccion;
+    // const { id_transaccion, ...pagoData } = data;
+    const data_new = {
+      idtransaccion: req.body.id_transaccion,
+      description: req.body.description,
+      token: req.body.token,
+      amount: req.body.amount,
+      cardType: req.body.cardType,
+      cardIssuer: req.body.cardIssuer,
+      cardInfo: req.body.cardInfo,
+      clientID: req.body.clientID,
+      clientName: req.body.clientName,
+      state: req.body.state,
+      fecha: req.body.fecha,
+      acquirer: req.body.acquirer,
+      deferred: req.body.deferred,
+      interests: req.body.interests,
+      interestValue: req.body.interestValue,
+      amountWoTaxes: req.body.amountWoTaxes,
+      amountWTaxes: req.body.amountWTaxes,
+      taxesValue: req.body.taxesValue,
+      amountAuthorized: req.body.amountAuthorized,
+      discountRate: req.body.discountRate,
+      extras: req.body.extras,
+      tipoPago: req.body.tipoPago
+    };
+    
 
     const newPago = await db.pagoplux.create({
-      data: pagoData,
+      data: data_new,
     });
 
     return res.status(200).json({ message: "success", newPago });
